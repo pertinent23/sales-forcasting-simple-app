f5428f35e5d749e48f22e16c5d5606d150a15a7f
pertinent23 <10franckpertinent@gmail.com>
2022-03-30 21:34:57 +0100

    after adding multiple file choosing

--- app.py
+++ app.py
@@ -14,14 +14,24 @@ def upload_file():
         if 'file' not in request.files:
             print("redirection")
             return redirect(request.url)
-        file = request.files.get('file')
+        file = request.files.get( 'file' )
+        file = file if file  else request.files.getList( 'file' )
         if not file:
             return
-        img_bytes = file.read()
-        class_name ,class_id = get_prediction(image_bytes=img_bytes)
-        return render_template('result.html', class_id=class_id,
-                               class_name=class_name)
-    return render_template('index.html')
+
+        file = request.files.get( 'file' )
+        if file:
+            img_bytes = file.read()
+            class_name ,class_id = get_prediction( image_bytes=img_bytes )
+            return render_template( 'result.html', list=[ { 'id': class_id, 'name': class_name } ] )
+        else: 
+            final = [ ]
+            for file in request.files.getList( 'file' ):
+                img_bytes = file.read()
+                class_name ,class_id = get_prediction( image_bytes=img_bytes )
+                final.append( { 'id': class_id, 'name': class_name } )
+            return render_template( 'result.html', list=final )
+    return render_template( 'index.html' )
 
 
 if __name__ == '__main__':
